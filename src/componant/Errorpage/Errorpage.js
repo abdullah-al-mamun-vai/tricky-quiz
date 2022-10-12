522864363f093f5510852a1cbfba46c22d9b87b9
abdullah-al-mamun-vai <mdm66304@gmail.com>
2022-10-12 22:52:22 +0600

    errorpage customize

--- src/componant/Errorpage/Errorpage.js
+++ src/componant/Errorpage/Errorpage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useRouteError } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSadCry } from '@fortawesome/free-solid-svg-icons'
 import Header from './../Header'
 const Errorpage = () => {
     const error = useRouteError();
@@ -7,9 +9,12 @@ const Errorpage = () => {
         <div>
             <Header></Header>
             <div className='flex flex-col items-center min-h-screen justify-center'>
-                <h1 className='lg:text-4xl sm:text-2xl md:text-3xl font-bold capitalize'>OOPS,Data Not found</h1>
-                <p>status: {error.status ? error.status : 'try again'}</p>
-                <p> {error.statusText ? error.statusText : 'try again'}</p>
+                <div>
+                    <p className='text-center text-white text-9xl'><FontAwesomeIcon icon={faSadCry} /></p>
+                    <h1 className='lg:text-4xl text-white sm:text-2xl md:text-3xl font-bold capitalize'>OOPS,Data Not found</h1>
+                </div>
+                <p className='text-white'>Status: {error.status ? error.status : 'try again'}</p>
+                <p className='text-white'> {error.statusText ? error.statusText : 'try again'}</p>
             </div>
         </div>
     );
